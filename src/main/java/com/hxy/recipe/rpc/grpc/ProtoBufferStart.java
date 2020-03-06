22a0bf3cdad1ae9f20c8362c2670e1adc4d47513
霍夏玉 <huoxiayu@xiaomi.com>
2020-03-06 18:23:35 +0800

    protobuffer start

--- src/main/java/com/hxy/recipe/rpc/grpc/ProtoBufferStart.java
+++ src/main/java/com/hxy/recipe/rpc/grpc/ProtoBufferStart.java
@@ -1,13 +1,46 @@
 package com.hxy.recipe.rpc.grpc;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.hxy.rpc.grpc.model.CompositeHello;
+import com.hxy.rpc.grpc.model.Gender;
 import com.hxy.rpc.grpc.model.HelloRequest;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Map;
+
+@Slf4j
 public class ProtoBufferStart {
 
     public static void main(String[] args) {
-        HelloRequest helloRequest = HelloRequest.newBuilder()
-            .setReqMsg("ping")
-            .build();
+        protoBuffer();
     }
 
+    private static void protoBuffer() {
+        CompositeHello serialize = CompositeHello.newBuilder()
+            .setMsg("msg")
+            .addMsgList("msgItem")
+            .setId(1)
+            .setHelloRequest(HelloRequest.newBuilder().setReqMsg("hello"))
+            .setSkip(false)
+            .addInfoList(CompositeHello.Info.newBuilder().setName("info").setTimestamp(System.currentTimeMillis()))
+            .setGender(Gender.MAN)
+            .putAllStrMap(Map.of("ext", "ext_info"))
+            .build();
+
+        log.info("serialize: {}", serialize);
+
+        byte[] bytes = serialize.toByteArray();
+        log.info("serialize bytes: {}", bytes);
+
+        try {
+            CompositeHello deserialize = CompositeHello.parseFrom(bytes);
+            log.info("deserialize: {}", deserialize);
+
+            log.info("consistent: {}", serialize == deserialize);
+
+            log.info("equals: {}", serialize.equals(deserialize));
+        } catch (InvalidProtocolBufferException e) {
+            e.printStackTrace();
+        }
+    }
 }
