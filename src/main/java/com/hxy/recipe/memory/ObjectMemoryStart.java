cfb86602f0f0385bafd2af69857c36670475ab54
huoxiayu <928748545@qq.com>
2020-10-13 13:18:08 +0800

    jol

--- src/main/java/com/hxy/recipe/memory/ObjectMemoryStart.java
+++ src/main/java/com/hxy/recipe/memory/ObjectMemoryStart.java
@@ -1,46 +1,35 @@
 package com.hxy.recipe.memory;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.lucene.util.RamUsageEstimator;
+import org.openjdk.jol.info.ClassLayout;
 
-import java.util.concurrent.ConcurrentHashMap;
-
+/**
+ * jol（java object layout）工具实践
+ */
 @Slf4j
 public class ObjectMemoryStart {
 
-    private static final int TIMES = 10_0000;
-    private static final Object VALUE = new Object();
+    private static class A {
 
-    private static class IntClass {
-        private int useless;
     }
 
-    private static class IntegerClass {
-        private Integer useless;
+    private static class B {
+        private long useless;
+    }
+
+    private static class C {
+        private boolean useless;
     }
 
     public static void main(String[] args) {
-        log.info("size of a object {}", RamUsageEstimator.humanSizeOf(new Object()));
-        log.info("size of a integer {}", RamUsageEstimator.humanSizeOf(100000));
-        log.info("size of a int[] {}", RamUsageEstimator.humanSizeOf(new int[10000]));
-        log.info("size of a object[] {}", RamUsageEstimator.humanSizeOf(new Object[10000]));
-        log.info("size of a IntClass[] {}", RamUsageEstimator.humanSizeOf(new IntClass[10000]));
-        log.info("size of a IntegerClass[] {}", RamUsageEstimator.humanSizeOf(new IntegerClass[10000]));
+        A a = new A();
+        log.info("a -> {}", ClassLayout.parseInstance(a).toPrintable());
 
-        performanceTestForConcurrentHashMap(new ConcurrentHashMap<>());
-        performanceTestForConcurrentHashMap(new ConcurrentHashMap<>(TIMES));
-    }
+        B b = new B();
+        log.info("b -> {}", ClassLayout.parseInstance(b).toPrintable());
 
-    private static void performanceTestForConcurrentHashMap(ConcurrentHashMap<Integer, Object> i2o) {
-        log.info("memory test start");
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < TIMES; i++) {
-            i2o.put(i * 100 + 31, VALUE);
-        }
-
-        long cost = System.currentTimeMillis() - start;
-        log.info("cost {} millis", cost);
-        log.info("size of concurrent hash map {}", RamUsageEstimator.humanSizeOf(i2o));
+        C c = new C();
+        log.info("c -> {}", ClassLayout.parseInstance(c).toPrintable());
     }
 
 }
