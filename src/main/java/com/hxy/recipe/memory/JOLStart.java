798627be7ad05abf19456f3ce50d7ae07c7c46a0
huoxiayu <928748545@qq.com>
2020-10-23 15:40:25 +0800

    jol and compressed class-pointers & oops

--- src/main/java/com/hxy/recipe/memory/JOLStart.java
+++ src/main/java/com/hxy/recipe/memory/JOLStart.java
@@ -5,6 +5,14 @@ import org.openjdk.jol.info.ClassLayout;
 
 /**
  * jol（java object layout）工具实践
+ * <p>
+ * 1、java -XX:+PrintCommandLineFlags
+ * 打印默认JVM参数，可以看到：
+ * -XX:+UseCompressedClassPointers -XX:+UseCompressedOops
+ * class-point占4个字节，oops占4个字节
+ * 关闭UseCompressedClassPointers后，class-point占8个字节
+ * 关闭UseCompressedOops后，oops占8个字节
+ * 关闭用：-XX:-UseCompressedClassPointers -XX:-UseCompressedOops
  */
 @Slf4j
 public class JOLStart {
@@ -23,6 +31,19 @@ public class JOLStart {
     }
 
     public static void main(String[] args) {
+        int[] oneDimArray = new int[16];
+        printObjectLayout(oneDimArray);
+
+        int[][] twoDimArray = new int[8][2];
+        for (int i = 0; i < 8; i++) {
+            twoDimArray[i] = new int[2];
+        }
+        printObjectLayout(twoDimArray);
+        printObjectLayout(twoDimArray[0][0]);
+
+        printObjectLayout(0);
+        printObjectLayout(0L);
+
         printObjectLayout(new A());
         printObjectLayout(new B());
         printObjectLayout(new C());
