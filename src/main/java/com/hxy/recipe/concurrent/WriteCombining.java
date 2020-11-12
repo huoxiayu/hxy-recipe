d122d60c8b8ebdd7d47457b1b9cd4b3912bb4fb0
huoxiayu <928748545@qq.com>
2020-11-12 10:54:55 +0800

    write combining

---
+++ src/main/java/com/hxy/recipe/concurrent/WriteCombining.java
@@ -0,0 +1,70 @@
+package com.hxy.recipe.concurrent;
+
+import com.hxy.recipe.util.TimeUtil;
+
+public class WriteCombining {
+
+    private static final int ITERATIONS = Integer.MAX_VALUE;
+    private static final int ITEMS = 1 << 24;
+    private static final int MASK = ITEMS - 1;
+
+    private static final byte[] arrayA = new byte[ITEMS];
+    private static final byte[] arrayB = new byte[ITEMS];
+    private static final byte[] arrayC = new byte[ITEMS];
+    private static final byte[] arrayD = new byte[ITEMS];
+    private static final byte[] arrayE = new byte[ITEMS];
+    private static final byte[] arrayF = new byte[ITEMS];
+
+    public static void main(final String[] args) {
+        for (int i = 1; i <= 3; i++) {
+            System.out.println(i + " SingleLoop duration (ms) = " + TimeUtil.nano2Millis(runCaseOne()));
+            System.out.println(i + " SplitLoop duration (ms) = " + TimeUtil.nano2Millis(runCaseTwo()));
+        }
+
+        int result = arrayA[1] + arrayB[2] + arrayC[3] + arrayD[4] + arrayE[5] + arrayF[6];
+
+        System.out.println("result = " + result);
+    }
+
+    public static long runCaseOne() {
+        long start = System.nanoTime();
+        int i = ITERATIONS;
+
+        while (--i != 0) {
+            int slot = i & MASK;
+            byte b = (byte) i;
+            arrayA[slot] = b;
+            arrayB[slot] = b;
+            arrayC[slot] = b;
+            arrayD[slot] = b;
+            arrayE[slot] = b;
+            arrayF[slot] = b;
+        }
+
+        return System.nanoTime() - start;
+    }
+
+    public static long runCaseTwo() {
+        long start = System.nanoTime();
+        int i = ITERATIONS;
+        while (--i != 0) {
+            int slot = i & MASK;
+            byte b = (byte) i;
+            arrayA[slot] = b;
+            arrayB[slot] = b;
+            arrayC[slot] = b;
+        }
+
+        i = ITERATIONS;
+
+        while (--i != 0) {
+            int slot = i & MASK;
+            byte b = (byte) i;
+            arrayD[slot] = b;
+            arrayE[slot] = b;
+            arrayF[slot] = b;
+        }
+
+        return System.nanoTime() - start;
+    }
+}
--- src/main/java/com/hxy/recipe/util/TimeUtil.java
+++ src/main/java/com/hxy/recipe/util/TimeUtil.java
@@ -9,12 +9,21 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class TimeUtil {
 
     private static final ZoneOffset CHINA_TIME_ZONE = ZoneOffset.ofHours(8);
+
+    public static long nano2Millis(long nanoseconds) {
+        return TimeUnit.NANOSECONDS.toMillis(nanoseconds);
+    }
+
+    public static long nano2Seconds(long nanoseconds) {
+        return TimeUnit.NANOSECONDS.toSeconds(nanoseconds);
+    }
 
     public static long atStartOfDay(LocalDate localDate) {
         return localDate.atStartOfDay(CHINA_TIME_ZONE).toInstant().toEpochMilli();
