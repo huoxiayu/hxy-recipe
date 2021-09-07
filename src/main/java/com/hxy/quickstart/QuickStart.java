fcc79634a4c10a7eee1ec574d6ffd786e1d15205
huoxiayu <928748545@qq.com>
2021-09-07 11:53:05 +0800

    secure key

--- src/main/java/com/hxy/quickstart/QuickStart.java
+++ src/main/java/com/hxy/quickstart/QuickStart.java
@@ -2,13 +2,41 @@ package com.hxy.quickstart;
 
 import com.hxy.recipe.util.Utils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.util.StopWatch;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 public class QuickStart {
 
     public static void main(String[] args) {
+        String client_id = "some_client_id";
+        String timestamp = String.valueOf(System.currentTimeMillis());
+        String nonce = String.valueOf(ThreadLocalRandom.current().nextLong(100));
+        String secure_key = "some_secure_key";
+
+        List<String> keys = new ArrayList<>();
+        keys.add(client_id);
+        keys.add(timestamp);
+        keys.add(nonce);
+        keys.add(secure_key);
+
+        Collections.sort(keys);
+
+        String joinKey = String.join("", keys);
+        String sign = DigestUtils.sha1Hex(joinKey);
+        System.out.println(sign);
+
+
+
+
+
+
         log.info("", ExceptionUtils.getStackTrace(new Exception()));
 
         StopWatch sw = new StopWatch();
