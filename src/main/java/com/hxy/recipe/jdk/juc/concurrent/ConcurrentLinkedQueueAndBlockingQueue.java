5cc8137ba0d11f5b6a428da3b8fff26554f78638
huoxiayu <928748545@qq.com>
2021-11-28 01:59:03 +0800

    thread safe queue

--- src/main/java/com/hxy/recipe/jdk/juc/concurrent/ConcurrentLinkedQueueAndBlockingQueue.java
+++ src/main/java/com/hxy/recipe/jdk/juc/concurrent/ConcurrentLinkedQueueAndBlockingQueue.java
@@ -4,34 +4,72 @@ import com.hxy.recipe.util.BenchmarkUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.spark_project.jetty.util.BlockingArrayQueue;
 
-import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
+/**
+ * [parallel -> 12]
+ * [per-size -> 10000]
+ * [linkedBlockingQueue cost 10 millis]
+ * [linkedBlockingQueue.size(): 120000]
+ * [concurrentLinkedQueue cost 15 millis]
+ * [concurrentLinkedQueue.size(): 120000]
+ * [blockingArrayQueue cost 189 millis]
+ * [blockingArrayQueue.size(): 120000]
+ */
 @Slf4j
 public class ConcurrentLinkedQueueAndBlockingQueue {
 
+    private static final int PARALLEL = Runtime.getRuntime().availableProcessors();
+    private static final int SIZE = 10000;
+
     public static void main(String[] args) {
-        int size = 10000;
-        String[] input = new String[size];
-        for (int i = 0; i < size; i++) {
+        run();
+        run();
+
+        try {
+            TimeUnit.SECONDS.sleep(5L);
+        } catch (InterruptedException ignore) {
+
+        }
+
+        run();
+    }
+
+    private static void run() {
+        String[] input = new String[SIZE];
+        for (int i = 0; i < SIZE; i++) {
             input[i] = String.valueOf(i);
         }
 
-        Queue<String> concurrentLinkedQueue = new ConcurrentLinkedQueue<>();
+        log.info("parallel -> {}", PARALLEL);
+        log.info("per-size -> {}", SIZE);
+
+        LinkedBlockingQueue<String> linkedBlockingQueue = new LinkedBlockingQueue<>();
+        long linkedBlockingQueueCost = BenchmarkUtil.multiRun(() -> {
+            for (int i = 0; i < SIZE; i++) {
+                linkedBlockingQueue.add(input[i]);
+            }
+        }, PARALLEL);
+        log.info("linkedBlockingQueue cost {} millis", linkedBlockingQueueCost);
+        log.info("linkedBlockingQueue.size(): {}", linkedBlockingQueue.size());
+
+        ConcurrentLinkedQueue<String> concurrentLinkedQueue = new ConcurrentLinkedQueue<>();
         long concurrentLinkedQueueCost = BenchmarkUtil.multiRun(() -> {
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < SIZE; i++) {
                 concurrentLinkedQueue.add(input[i]);
             }
-        }, 20);
+        }, PARALLEL);
         log.info("concurrentLinkedQueue cost {} millis", concurrentLinkedQueueCost);
         log.info("concurrentLinkedQueue.size(): {}", concurrentLinkedQueue.size());
 
-        Queue<String> blockingArrayQueue = new BlockingArrayQueue<>();
+        BlockingArrayQueue<String> blockingArrayQueue = new BlockingArrayQueue<>();
         long blockingArrayQueueCost = BenchmarkUtil.multiRun(() -> {
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < SIZE; i++) {
                 blockingArrayQueue.add(input[i]);
             }
-        });
+        }, PARALLEL);
         log.info("blockingArrayQueue cost {} millis", blockingArrayQueueCost);
         log.info("blockingArrayQueue.size(): {}", blockingArrayQueue.size());
     }
