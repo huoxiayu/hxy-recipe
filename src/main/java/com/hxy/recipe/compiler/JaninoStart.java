03de9571df02d7fbce7f95c39d841e70989c52ae
huoxiayu <928748545@qq.com>
2022-02-16 20:38:13 +0800

    Janino Compiler

---
+++ src/main/java/com/hxy/recipe/compiler/JaninoStart.java
@@ -0,0 +1,15 @@
+package com.hxy.recipe.compiler;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Janino Compiler Used By Spark/Flink/Presto
+ */
+@Slf4j
+public class JaninoStart {
+
+    public static void main(String[] args) {
+
+    }
+
+}
