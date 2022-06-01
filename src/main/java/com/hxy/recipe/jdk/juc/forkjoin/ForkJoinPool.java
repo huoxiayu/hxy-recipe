f3577ad3042181f7ce4e9402bc70923f89799ba4
huoxiayu <928748545@qq.com>
2022-06-01 16:41:37 +0800

    fjp

--- src/main/java/com/hxy/recipe/jdk/juc/forkjoin/ForkJoinPool.java
+++ src/main/java/com/hxy/recipe/jdk/juc/forkjoin/ForkJoinPool.java
@@ -1,5 +1,6 @@
 package com.hxy.recipe.jdk.juc.forkjoin;
 
+import cn.hutool.core.collection.ConcurrentHashSet;
 import com.hxy.recipe.util.BenchmarkUtil;
 import com.hxy.recipe.util.Utils;
 import lombok.extern.slf4j.Slf4j;
@@ -11,26 +12,35 @@ import java.util.stream.IntStream;
 public class ForkJoinPool {
 
     static {
-        int parallel = 1;
+        int parallel = 1000;
         System.setProperty(
                 "java.util.concurrent.ForkJoinPool.common.parallelism",
                 String.valueOf(parallel)
         );
+        log.info("set parallel -> {}", parallel);
     }
 
     public static void main(String[] args) {
-        BenchmarkUtil.singleRun(() -> {
-            AtomicInteger cnt = new AtomicInteger();
-            IntStream.iterate(1, i -> i + 1)
-                    .limit(50L)
-                    .parallel()
-                    .forEach(any -> {
-                        Utils.sleepInSeconds(1L);
-                        cnt.incrementAndGet();
-                        log.info("process end");
-                    });
-            log.info("cnt -> {}", cnt);
-        }, "total process");
+        while (true) {
+            ConcurrentHashSet<String> set = new ConcurrentHashSet<>();
+            BenchmarkUtil.singleRun(() -> {
+                AtomicInteger cnt = new AtomicInteger();
+                IntStream.iterate(1, i -> i + 1)
+                        .limit(5000L)
+                        .parallel()
+                        .forEach(any -> {
+                            Utils.sleepInMillis(30L);
+                            cnt.incrementAndGet();
+                            // log.info("process end");
+                            set.add(Thread.currentThread().getName());
+                        });
+                log.info("cnt -> {}", cnt);
+            }, "total process");
+            log.info("set.size() -> {}", set.size());
+
+            System.gc();
+            Utils.sleepInSeconds(5L);
+        }
     }
 
 }
