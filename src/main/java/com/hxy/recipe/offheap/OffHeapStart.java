cf9ce811237d31254b5cbf85c5e7ba54d52cd657
霍夏玉 <huoxiayu@xiaomi.com>
2022-07-22 17:00:05 +0800

    off-heap

--- src/main/java/com/hxy/recipe/offheap/OffHeapStart.java
+++ src/main/java/com/hxy/recipe/offheap/OffHeapStart.java
@@ -1,9 +1,48 @@
 package com.hxy.recipe.offheap;
 
+import com.hxy.recipe.util.BenchmarkUtil;
+import com.hxy.recipe.util.RunnableUtil;
+import com.hxy.recipe.util.Utils;
+
+import java.nio.ByteBuffer;
+
 public class OffHeapStart {
 
-    public static void main(String[] args) {
+    private static final int SIZE = 128 * 1024 * 1024;
+    private static final int TIMES = 10;
+    private static final ByteBuffer DIRECT_BYTE_BUFFER = ByteBuffer.allocateDirect(SIZE);
+    private static final ByteBuffer HEAP_BYTE_BUFFER = ByteBuffer.allocate(SIZE);
 
+    public static void main(String[] args) {
+        while (true) {
+            BenchmarkUtil.singleRun(
+                    RunnableUtil.loopRunnable(() -> {
+                        byte sum = 0;
+                        for (int i = 0; i < SIZE; i++) {
+                            sum += DIRECT_BYTE_BUFFER.get(i);
+                        }
+                        if (sum != 0) {
+                            throw new IllegalStateException();
+                        }
+                    }, TIMES)
+                    , "off-heap-cost"
+            );
+
+            BenchmarkUtil.singleRun(
+                    RunnableUtil.loopRunnable(() -> {
+                        byte sum = 0;
+                        for (int i = 0; i < SIZE; i++) {
+                            sum += HEAP_BYTE_BUFFER.get(i);
+                        }
+                        if (sum != 0) {
+                            throw new IllegalStateException();
+                        }
+                    }, TIMES)
+                    , "on-heap-cost"
+            );
+
+            Utils.sleepInSeconds(1L);
+        }
     }
 
 }
--- src/main/java/com/hxy/recipe/performance/SortPerformance.java
+++ src/main/java/com/hxy/recipe/performance/SortPerformance.java
@@ -4,24 +4,35 @@ import com.hxy.recipe.util.BenchmarkUtil;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Slf4j
 public class SortPerformance {
 
-    private static final int TIMES = 30_0000;
+    private static final int TIMES = 100_0000;
 
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < TIMES; i++) {
-            list.add(ThreadLocalRandom.current().nextInt(TIMES));
-        }
-        log.info("add cost {} millis", System.currentTimeMillis() - start);
+        while (true) {
+            long start = System.currentTimeMillis();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < TIMES; i++) {
+                list.add(ThreadLocalRandom.current().nextInt(TIMES));
+            }
+            log.info("add cost {} millis", System.currentTimeMillis() - start);
 
-        long cost = BenchmarkUtil.singleRun(() -> list.sort(Integer::compare));
-        log.info("sort cost {} millis", cost);
+            long cost = BenchmarkUtil.singleRun(() -> list.sort(Integer::compare));
+            log.info("sort cost {} millis", cost);
+
+            int[] intArray = new int[TIMES];
+            for (int i = 0; i < TIMES; i++) {
+                intArray[i] = ThreadLocalRandom.current().nextInt(TIMES);
+            }
+
+            long intArraySortCost = BenchmarkUtil.singleRun(() -> Arrays.sort(intArray));
+            log.info("intArraySort cost {} millis", intArraySortCost);
+        }
     }
 
 }
