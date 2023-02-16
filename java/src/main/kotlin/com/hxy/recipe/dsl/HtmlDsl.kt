bd93428bbef2ea59b45cf9f200d01a17fcb0fe06
霍夏玉 <huoxiayu@xiaomi.com>
2023-02-16 16:19:36 +0800

    rm repeat class

--- java/src/main/kotlin/com/hxy/recipe/dsl/HtmlDsl.kt
+++ java/src/main/kotlin/com/hxy/recipe/dsl/HtmlDsl.kt
@@ -4,9 +4,6 @@ import kotlinx.html.*
 import kotlinx.html.dom.createHTMLDocument
 import kotlinx.html.dom.serialize
 
-class HtmlDsl {
-}
-
 fun main() {
     val tree = createHTMLDocument().html {
         body {
