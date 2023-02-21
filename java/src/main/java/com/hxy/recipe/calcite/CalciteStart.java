de291d26424f6fde7706cd533a8e93d6ca1088a5
霍夏玉 <huoxiayu@xiaomi.com>
2023-02-21 17:25:50 +0800

    CalciteStart

--- java/src/main/java/com/hxy/recipe/calcite/CalciteStart.java
+++ java/src/main/java/com/hxy/recipe/calcite/CalciteStart.java
@@ -1,12 +1,18 @@
 package com.hxy.recipe.calcite;
 
+import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.dialect.CalciteSqlDialect;
+import org.apache.calcite.sql.parser.SqlParser;
+
 // https://calcite.apache.org/docs/tutorial.html
 // https://calcite.apache.org/docs/stream.html
 // https://calcite.apache.org/develop/#source-code
 public class CalciteStart {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        SqlParser sqlParser = SqlParser.create("select * from tb");
+        SqlNode sqlNode = sqlParser.parseStmt();
+        System.out.println(sqlNode);
     }
 
 }
