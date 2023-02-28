324f2f3d0bfdf098de76a5dcdb46bcbf7644b192
霍夏玉 <huoxiayu@xiaomi.com>
2023-02-28 20:39:18 +0800

    cpp: typeinfo

--- cpp/cpp_recipe/typeinfo/about_typeinfo.cpp
+++ cpp/cpp_recipe/typeinfo/about_typeinfo.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+#include <cxxabi.h>
 #include <iostream>
 #include <typeinfo>
 using namespace std;
@@ -26,8 +28,32 @@ struct Struct2 {
     int32_t i;
 };
 
+template <typename T> string get_type_name() {
+    string tname = typeid(T).name();
+    int status;
+    char *demangled_name =
+        abi::__cxa_demangle(tname.c_str(), NULL, NULL, &status);
+    if (status == 0) {
+        tname = demangled_name;
+        std::free(demangled_name);
+    }
+    return tname;
+}
+
 int main() {
     cout << "begin" << endl;
+
+    cout << "get_type_name<int>(): " << get_type_name<int>() << endl;
+    cout << "get_type_name<int32_t>(): " << get_type_name<int32_t>() << endl;
+    cout << "get_type_name<int64_t>(): " << get_type_name<int64_t>() << endl;
+    cout << "get_type_name<long>(): " << get_type_name<long>() << endl;
+    cout << "get_type_name<bool>(): " << get_type_name<bool>() << endl;
+    cout << "get_type_name<string>(): " << get_type_name<string>() << endl;
+    cout << "get_type_name<char *>(): " << get_type_name<char *>() << endl;
+    cout << "get_type_name<Struct1>(): " << get_type_name<Struct1>() << endl;
+    cout << "get_type_name<Struct2>(): " << get_type_name<Struct2>() << endl;
+    cout << "get_type_name<decltype(add)>(): " << get_type_name<decltype(add)>()
+         << endl;
 
     int a = 1;
     long b = 2;
@@ -41,5 +67,6 @@ int main() {
 
     print_all(a, b, c, d, e, f, g, add, h, i);
     cout << "end" << endl;
+
     return 0;
 }
