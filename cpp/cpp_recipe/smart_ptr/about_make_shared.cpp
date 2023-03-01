b3b9072d51a327bbeb4af268fce94b876696f61d
霍夏玉 <huoxiayu@xiaomi.com>
2023-03-01 20:47:15 +0800

    about_internal_of_make_shared

--- cpp/cpp_recipe/smart_ptr/about_make_shared.cpp
+++ cpp/cpp_recipe/smart_ptr/about_make_shared.cpp
@@ -24,6 +24,24 @@ struct S {
     }
 };
 
+// TODO
+void about_internal_of_make_shared() {
+    using _ControlBlock = __shared_ptr_emplace<S, allocator<S>>;
+
+    using _ControlBlockAllocator =
+        typename __allocator_traits_rebind<allocator<S>, _ControlBlock>::type;
+
+    const allocator<S> &__a = allocator<S>();
+
+    __allocation_guard<_ControlBlockAllocator> __guard(__a, 1);
+
+    ::new ((void *)_VSTD::addressof(*__guard.__get())) _ControlBlock(__a);
+
+    auto __control_block = __guard.__release_ptr();
+    auto ret = shared_ptr<S>::__create_with_control_block(
+        (*__control_block).__get_elem(), _VSTD::addressof(*__control_block));
+}
+
 int main() {
     cout << "make_shared<S>()->v: " << make_shared<S>()->v << endl;
 
@@ -38,6 +56,7 @@ int main() {
     cout << "&s: " << &s << endl;
 
     shared_ptr<S> s_ptr = make_shared<S>(s);
+
     // 堆地址
     cout << "s_ptr is: " << s_ptr << endl;
 
