f474124a5ca73f8e6f8dd11e8674db423d0ce3eb
霍夏玉 <huoxiayu@xiaomi.com>
2023-03-02 20:13:07 +0800

    cpp: thread

--- cpp/cpp_recipe/multi_thread/thread/about_thread.cpp
+++ cpp/cpp_recipe/multi_thread/thread/about_thread.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <iostream>
+#include <pthread.h>
 #include <thread>
+
 using namespace std;
 
 void foo() {
@@ -17,6 +19,19 @@ void bar(uint32_t seconds) {
     cout << this_id << " bar end " << endl;
 }
 
+void thread_property(string th_name) {
+    const int len = 32;
+    char thread_name[len];
+
+    pthread_getname_np(pthread_self(), thread_name, len);
+    cout << "thread name is: " << thread_name << endl;
+
+    pthread_setname_np(pthread_self(), th_name.c_str());
+
+    pthread_getname_np(pthread_self(), thread_name, len);
+    cout << "thread name is: " << thread_name << endl;
+}
+
 int main() {
     cout << "begin" << endl;
 
@@ -25,6 +40,12 @@ int main() {
 
     thread1.join();
     thread2.join();
+
+    thread thread3(thread_property, "thread_name_x");
+    thread3.join();
+
+    thread thread4(thread_property, "thread_name_y");
+    thread4.join();
 
     cout << "end" << endl;
     return 0;
